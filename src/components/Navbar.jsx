7ecdb971cea8535ed3098ee63f566c787896a6e8
unknown <harshsinghlay9990@gmail.com>
2026-08-14 00:42:34 +0530

    Add apology website

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav className="w-full px-8 py-4 flex items-center justify-between bg-white/40 backdrop-blur-md shadow-sm">
+      <h1 className="text-2xl font-semibold text-rose-600">
+        To Prachi babe 💗
+      </h1>
+
+      <p className="text-lg text-purple-600 font-medium">
+      To cutiee 💕
+      </p>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/data/story.js
@@ -0,0 +1,36 @@
+import img1 from "../assets/images/img1.jpg";
+import img2 from "../assets/images/img2.jpg";
+import img3 from "../assets/images/img3.jpg";
+import img4 from "../assets/images/img4.jpg";
+import img5 from "../assets/images/img5.jpg";
+
+export const story = [
+  {
+    image: img1,
+    text: "I HAVE SOMETHING TO SAY TO YOU...",
+  },
+  {
+    image: img2,
+    text: "Okay... I know you're upset with me. And I understand why.I know saving your voice note without properly thinking about how you'd feel wasn't okay.✨",
+  },
+  {
+    image: img3,
+    text: "I'm not going to make excuses.I should have respected your privacy and thought about your comfort first.I genuinely didn't mean to make you feel like I don't respect your trust.But I understand that what I meant doesn't change how it made you feel. 🥺",
+  },
+  {
+    image: img4,
+    text: "And honestly... The last thing I want is for this silly mistake to make things weird between us. I really value what we have. You're important to me, and I don't want my stupidity to become a reason for ..... between us. Pr is bat pe tumhare hatho ke do char chate to khane h mujhe. Do char lat ghuse jo dena chaho. Dekkho ye mt sochna ki main needy sound krra hu pr mujhe sachme ye chahiye. 😌",
+  },
+  {
+    image: img5,
+    text: "I'm genuinely sorry. ❤️ You don't have to stop being angry just because I'm saying sorry. Take your time. I just wanted you to know that I understand, and I'll respect your boundaries better .❤️",
+  },
+  {
+    image: img5,
+    text: "Okay, I'm done now. 🥺 No more explanations. No pressure on you ki tum man jao ek din me. Just one stupid boy admitting that he messed up. I'm sorry.And whenever you're ready... I'm here for u. ❤️❤️",
+  },
+  {
+    image: img5,
+    text: "May today be a perfect excuse to remind you how important you are and how much you are valued. Thank you for being here, for contributing, and for making everything more beautiful.❤️❤️",
+  },
+];
